b153d0b7a0d2f971f5723477f41b6119a8edd903
hishemok <hishemok@uio.no>
2024-09-11 13:33:24 +0200

    Fixing all tests

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,19 @@ import numpy as np
 
 
 def differentiate(u, dt):
-    pass
+    diff = np.zeros(len(u))
+    diff[0] = (u[1] - u[0]) / dt
+    diff[-1] = (u[-1] - u[-2]) / dt
+    for i in range(1, len(u)-1):
+        diff[i] = (u[i+1] - u[i-1]) / (2*dt)
+    return diff
 
 def differentiate_vector(u, dt):
-    pass
+    diff = np.zeros(len(u))
+    diff[0] = (u[1] - u[0]) / dt
+    diff[-1] = (u[-1] - u[-2]) / dt
+    diff[1:-1] = (u[2:] - u[:-2]) / (2*dt)
+    return diff
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -2,13 +2,20 @@ import numpy as np
 
 
 def mesh_function(f, t):
-    pass
+    return np.array([f(i) for i in t])
 
 def func(t):
-    pass
+    if t <= 3 & t > 0:
+        return np.exp(-t)
+    elif t > 3 & t <= 4:
+        return np.exp(-3*t)
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
     f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
     fun = mesh_function(func, t)
     assert np.allclose(fun, f)
+
+if __name__ == '__main__':
+    test_mesh_function()
+
